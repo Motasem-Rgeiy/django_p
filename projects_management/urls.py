4db81e27c1798882540ec7808a1c21da68fb5e92
motasem <muohtassomerghei@gmail.com>
2026-01-20 17:41:26 +0200

    Admin Panel

--- projects_management/urls.py
+++ projects_management/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.utils.translation import gettext as _
+
+admin.site.site_header = _('Project Management')
+admin.site.site_title = _('Project Management')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
